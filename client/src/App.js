7186f99bbe6c283d91037b342c56eb71418628be
marvin lara <laramarvin105@gmail.com>
2019-12-09 19:15:52 -0500

    added css

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import Section from "./components/section/Section";
+import Content from "./components/content/Content";
+function App() {
+  return (
+    <div className="App">
+        <Section />
+        <Content />
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/components/content/Content.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import './style.css';
+
+function Content(){
+    return(
+        <div className="content">
+            <h3 className="h3"> 
+                I have a diverse set of skills that can be intergrated on the front-end or the back-end of developing a website. </h3>
+                <br />
+            
+                <div>
+                    <h4 className="h3">Let's work together</h4>
+                    <div className="links"> 
+                        
+                    </div>
+                </div>
+
+                <br />
+            <div className="images">
+                    <img src="/images/mongodb.jpg"/>
+                    <img src="/images/express.png"/>
+                    <img src="/images/react.jpg"/>
+                    <img src="/images/node.jpg"/>
+                    <img src="/images/js.jpg"/>
+                    <img src="/images/mysql.jpg"/>
+            </div>
+        </div>
+    )
+}
+
+export default Content;
