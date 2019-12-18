43a553414b02f091abfc164ac018bd7bf3db271c
marvin lara <laramarvin105@gmail.com>
2019-12-18 10:26:28 -0500

    Updates

---
+++ precache-manifest.642eb6f7a2f2294641143aa4850a278d.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e26593b515ce369e6398cc8e62bf8b2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3239ee313cb2089adbde",
+    "url": "/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "bdc3ad757c32982330cb",
+    "url": "/static/css/main.c9676da9.chunk.css"
+  },
+  {
+    "revision": "3239ee313cb2089adbde",
+    "url": "/static/js/2.26a2645d.chunk.js"
+  },
+  {
+    "revision": "86afaa925e0f432774bf95b19b6933fb",
+    "url": "/static/js/2.26a2645d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "bdc3ad757c32982330cb",
+    "url": "/static/js/main.a11ab991.chunk.js"
+  },
+  {
+    "revision": "2bcb8084869f23f72706",
+    "url": "/static/js/runtime-main.88d4f3f8.js"
+  },
+  {
+    "revision": "e36e163adaba71d5a6441ec0499829e1",
+    "url": "/static/media/everest.e36e163a.png"
+  },
+  {
+    "revision": "98ff4822d03ab0f5746653f56441d38e",
+    "url": "/static/media/express.98ff4822.png"
+  },
+  {
+    "revision": "03cbde693a391ec3eaec7a535a6b4231",
+    "url": "/static/media/js.03cbde69.jpg"
+  },
+  {
+    "revision": "f1c1c3f1ec8ed68dab43a02e415c3c9f",
+    "url": "/static/media/me.f1c1c3f1.JPG"
+  },
+  {
+    "revision": "36e755beb469cf90d32bff3ad66d2379",
+    "url": "/static/media/mongodb.36e755be.jpg"
+  },
+  {
+    "revision": "5fc2eb804f1b1364d5e5549e07819e09",
+    "url": "/static/media/mysql.5fc2eb80.jpg"
+  },
+  {
+    "revision": "41911525f41992aa6501c6aa2c18dc3b",
+    "url": "/static/media/node.41911525.jpg"
+  },
+  {
+    "revision": "a47b04bb1da1f30737fb234240dc33d0",
+    "url": "/static/media/react.a47b04bb.jpg"
+  }
+]);
