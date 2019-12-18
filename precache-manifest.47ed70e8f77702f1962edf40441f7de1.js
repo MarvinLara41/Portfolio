bec2b0b38a5225d1aa2f263438ba40425cd42496
marvin lara <laramarvin105@gmail.com>
2019-12-18 11:23:18 -0500

    Updates

---
+++ precache-manifest.47ed70e8f77702f1962edf40441f7de1.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "84e3a27769e60fb5596fe9fa777da4f3",
+    "url": "/Protfolio/index.html"
+  },
+  {
+    "revision": "3239ee313cb2089adbde",
+    "url": "/Protfolio/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "f3949539b2a635446690",
+    "url": "/Protfolio/static/css/main.a824d2e7.chunk.css"
+  },
+  {
+    "revision": "3239ee313cb2089adbde",
+    "url": "/Protfolio/static/js/2.26a2645d.chunk.js"
+  },
+  {
+    "revision": "86afaa925e0f432774bf95b19b6933fb",
+    "url": "/Protfolio/static/js/2.26a2645d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f3949539b2a635446690",
+    "url": "/Protfolio/static/js/main.53f10613.chunk.js"
+  },
+  {
+    "revision": "f1f87c1425a61fdb3657",
+    "url": "/Protfolio/static/js/runtime-main.178c41aa.js"
+  },
+  {
+    "revision": "e36e163adaba71d5a6441ec0499829e1",
+    "url": "/Protfolio/static/media/everest.e36e163a.png"
+  },
+  {
+    "revision": "98ff4822d03ab0f5746653f56441d38e",
+    "url": "/Protfolio/static/media/express.98ff4822.png"
+  },
+  {
+    "revision": "03cbde693a391ec3eaec7a535a6b4231",
+    "url": "/Protfolio/static/media/js.03cbde69.jpg"
+  },
+  {
+    "revision": "f1c1c3f1ec8ed68dab43a02e415c3c9f",
+    "url": "/Protfolio/static/media/me.f1c1c3f1.JPG"
+  },
+  {
+    "revision": "36e755beb469cf90d32bff3ad66d2379",
+    "url": "/Protfolio/static/media/mongodb.36e755be.jpg"
+  },
+  {
+    "revision": "5fc2eb804f1b1364d5e5549e07819e09",
+    "url": "/Protfolio/static/media/mysql.5fc2eb80.jpg"
+  },
+  {
+    "revision": "41911525f41992aa6501c6aa2c18dc3b",
+    "url": "/Protfolio/static/media/node.41911525.jpg"
+  },
+  {
+    "revision": "a47b04bb1da1f30737fb234240dc33d0",
+    "url": "/Protfolio/static/media/react.a47b04bb.jpg"
+  }
+]);
